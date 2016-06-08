0db03a5cff023f7074fdb9700f256bb536528680
jonpol01 <johnpaul@soliva.me>
2016-06-08 23:55:16 +0900

    Initial Commit

---
+++ Sockets.Monitor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sockets.Monitor
+//
+//  Created by John Paul Soliva on 6/8/16.
+//  Copyright © 2016 Soliva John Paul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
